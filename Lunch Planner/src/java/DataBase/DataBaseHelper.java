d1d96f32e08a471f26d38cabec9113294fdfd8b3
IgorSudijovski@gmail.com <IgorSudijovski@gmail.com@549e8962-073f-b94f-1601-957477768f0e>
2012-01-04 16:50:24 +0000

    Implementacija na nekoi get metodi za rabota so bazata

--- Lunch Planner/src/java/DataBase/DataBaseHelper.java
+++ Lunch Planner/src/java/DataBase/DataBaseHelper.java
@@ -77,7 +77,7 @@ public class DataBaseHelper {
     
     public static List<List<String>> getAllNamesSNamesUsersEmailsPass() throws SQLException
     {
-        List<List<String>> lst = new ArrayList<List<String>>();;
+        List<List<String>> lst = new ArrayList<List<String>>();
             for(int i = 1; i<=5; i++)
                 lst.add(GetQuery("SELECT * FROM korisnik", i));
         return lst;
@@ -85,22 +85,58 @@ public class DataBaseHelper {
     
     public static boolean CheckUser(String user, String password)
     {
-        return true;
+        List<String> lst = GetQuery("select * from korisnik where User = '" + user + "'", 5);
+        if(password.equals(lst.get(0)))
+            return true;
+        return false;
     }
-    public static List<List<String>> getUserAndLunch(String ID_Group)
+    
+    public static List<String> GetAllGroups()
     {
-        List<List<String>> lst = null;
+        List<String> lst = GetQuery("select * from tekovnagrupa", 1);
         return lst;
+    }
+    public static List<List<String>> getNameSNameAndLunch(String ID_Group)
+    {
+        List<List<String>> lst = new ArrayList<List<String>>();
+        List<String> users = GetQuery("select Korisnik_User from tekovnagrupa,naracka where idTekovnaGrupa = TekovnaGrupa_idTekovnaGrupa And idTekovnaGrupa = '" + ID_Group + "'", 0);
+        List<String> LucnhID = GetQuery("select StavkaMeni_idStavkaMeni from tekovnagrupa,naracka where idTekovnaGrupa = TekovnaGrupa_idTekovnaGrupa And idTekovnaGrupa = '" + ID_Group + "'", 0);
+        List<String> Ime = new ArrayList<String>();
+        List<String> Prezime = new ArrayList<String>();
+        List<String> Lucnh = new ArrayList<String>();
+        for(int i = 0; i<users.size(); i++)
+        {
+            Ime.add(getUserIme(users.get(i)));
+            Prezime.add(getUserPrezime(users.get(i)));
+        }
+        for(int i = 0; i<LucnhID.size(); i++)
+        {
+            Lucnh.add(getLunch(LucnhID.get(i)));
+        }
+        lst.add(Ime);
+        lst.add(Prezime);
+        lst.add(Lucnh);
+        return lst;
+    }
+    
+    public static String getLunch(String ID_Stavka)
+    {
+        List<String> lst = (GetQuery("select * from stavkameni WHERE idStavkaMeni = '" + ID_Stavka + "'", 2));
+        if(!lst.isEmpty())
+            return lst.get(0);
+        return new String();
     }
     public static String getPreferencesHour(String user)
     {
-        String str = null;
-        return str;
+        List<String> lst = (GetQuery("select * from preferences where Korisnik_User = '" + user + "'", 1));
+        if(!lst.isEmpty())
+            return lst.get(0);
+        return new String();
     }
-    public static String getPreferencesParticipant(String user)
+    public static List<String> getPreferencesParticipant(String user)
     {
-        String str = null;
-        return str;
+        List<String> lst = GetQuery("select korisnik_User from korisnik_has_preferences where preferences_Korisnik_User = '" + user + "'", 1);
+        return lst;
     }
     public static String getPreferencesMeal(String user)
     {
@@ -118,26 +154,20 @@ public class DataBaseHelper {
     }
     public static List<String> getAllUsernames()
     {
-        List<String> lst = new ArrayList<String>();
-        lst = GetQuery("select * from korisnik", 3);
+        List<String> lst  = GetQuery("select * from korisnik", 3);
         return lst;
     }
     public static List<String> getAllRestaurantNames()
     {
-        List<String> lst = new ArrayList<String>();
-        lst = GetQuery("select * from restoran", 1);
+        List<String> lst  = GetQuery("select * from restoran", 1);
         return lst;
     }
-    public static List<String> getAllMenuItems(String RestaurantNames)
+    public static List<List<String>> getAllMenuItemsAndPrice(String RestaurantName)
     {
-        List<String> lst = null;
-        if("".equals(RestaurantNames))
-            lst = new ArrayList<String>();
-        else
-        {
-            /* IGOR TVOJOT KOD TUKA SAMO DA NE VRAKJA NULL NIKOGAS*/
-        }
-            
+        List<List<String>> lst = new ArrayList<List<String>>();
+        lst.add(GetQuery("select Ime from meni,stavkameni where idStavkaMeni = StavkaMeni_idStavkaMeni And Restoran_Ime = '" + RestaurantName + "'", 1));
+        List<String> Ceni = GetQuery("select * from meni where Restoran_Ime = '" + RestaurantName + "'", 2);
+        lst.add(Ceni);
         return lst;
     }
     public static String getUserIme (String username)
