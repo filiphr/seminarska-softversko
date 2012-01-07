046a2fd004adcd078dc6d8c6c69a9c36bbc7c862
mdocevski@gmail.com <mdocevski@gmail.com@549e8962-073f-b94f-1601-957477768f0e>
2012-01-07 17:49:18 +0000

    hederot skoro gotov

---
+++ Lunch Planner/src/java/Group/CreateNastan.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Group;
+
+import DataBase.DataBaseHelper;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Home
+ */
+public class CreateNastan extends HttpServlet {
+
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CreateNastan</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet CreateNastan at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+             */
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("Create.jsp");
+        rd.forward(request, response);
+    }
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String restorant = request.getParameter("ImeRestorant");
+        String vreme = request.getParameter("Vreme");
+        HttpSession ses = request.getSession();
+        String user = null;
+        synchronized (ses)
+        {
+            user = (String) ses.getAttribute("username");
+        }
+        if(user!=null && restorant!=null && vreme!=null)
+        {
+            DataBaseHelper.insertTekovnaGrupa(vreme, user, restorant);
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("Create.jsp");
+        rd.forward(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
