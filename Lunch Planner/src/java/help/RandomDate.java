ee160272f4bb7f5875241341e578cf4082a29082
igorsudijovski@gmail.com <igorsudijovski@gmail.com@549e8962-073f-b94f-1601-957477768f0e>
2012-08-30 16:34:50 +0000

    added random generator for insert in to database

--- Lunch Planner/src/java/help/RandomDate.java
+++ Lunch Planner/src/java/help/RandomDate.java
@@ -15,18 +15,12 @@ public class RandomDate {
     
     public static String getRandomDate(){
         int year = RandomDate.randBetween(2011, 2012);
-
         int month = RandomDate.randBetween(0, 11);
-
         GregorianCalendar gc = new GregorianCalendar(year, month, 1);
-
         int day = RandomDate.randBetween(1, gc.getActualMaximum(GregorianCalendar.DAY_OF_MONTH));
-
-        gc.set(year, month, day);
-        
+        gc.set(year, month, day);        
         DecimalFormat nft = new DecimalFormat("#00.###");
         nft.setDecimalSeparatorAlwaysShown(false);
-
         return nft.format(gc.get(GregorianCalendar.YEAR)) + "-" + nft.format(gc.get(GregorianCalendar.MONTH)) + "-" + nft.format(gc.get(GregorianCalendar.DAY_OF_MONTH));
     }
     public static int randBetween(int start, int end) {
